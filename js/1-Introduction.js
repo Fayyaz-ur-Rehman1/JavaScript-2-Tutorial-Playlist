cff5cde63b9cd658f17bcd7f55c56d3dee470118
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-08 01:41:46 +0530

    Introduction

---
+++ js/1-Introduction.js
@@ -0,0 +1,7 @@
+alert("my name is fayyazurrehman")
+  
+  // document.getElementById("h").innerHTML = "JavaScript-Tutorial-||";
+   document.write("fayyazurrehman")
+   console.log("hello world")
+   console.log(5+6);
+   alert(5+5)
