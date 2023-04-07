47f30b90e899519cab2f2c26837179e901a2756b
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-08 04:05:13 +0530

    Data-Type-in-JavaScript

---
+++ js/5-Data-Types-in-javascript.js
@@ -0,0 +1,58 @@
+// DATA TYPE IN JAVASCRIPT
+
+//  TypeOf
+let ty = typeof 24;
+console.log(ty);
+let typ = typeof(true);
+console.log(typ)
+
+    // String 
+    let a = "fayyaz urrehman";
+console.log(a);
+let b = "fay'yaz";
+console.log(b);
+let c = 'hell"o guys';
+console.log(c);
+let d = "How are \" u brother";
+console.log(d);
+
+// Number
+let e = 23;
+console.log(e);
+let f = 23.34;
+console.log(f);
+var g = -24.34;
+console.log(g);
+
+// Boolean
+let h = 2 < 3;
+console.log(h);
+let i = 2 > 3;
+console.log(i);
+
+// Array
+let j = ["fayyaz", "danish", "uzair", "adnan"];
+console.log(j)
+let k = new Array("fayyaz", "danish", "uzair", "adnan");
+console.log(k)
+
+//Object
+let l = {
+    name: 'fayyaz',
+    age: 17
+}
+console.log(l)
+console.log(l.name + l.age)
+
+// Undefind
+let m;
+console.log(m)
+
+// ---------------------------------------------------
+
+let n = "Fayyaz";
+alert("Hello"+ n);
+
+let o = "fayyaz";
+let p =  `Hello  ${o}`
+console.log(p)
