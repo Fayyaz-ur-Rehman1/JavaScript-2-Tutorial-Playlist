c135066116e6f6dee469f557460fa4e8fe753a3f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-08 02:41:56 +0530

    Variable

---
+++ js/3-Variable.js
@@ -0,0 +1,35 @@
+// variable  => var and let and const is a variable type and a b c is a variable name and 13 is a variable value
+var a = 13;
+a = 55;
+console.log(a)
+
+var a = "fayyaz";
+console.log(a);
+
+let b = 24;
+b = 55;
+console.log(b);
+
+// let b = 33;
+// console.log(b)
+
+const c = 67;
+console.log(c);
+
+// const h = "fayyazurehamn"
+// h = "khilji"
+// console.log(h)
+
+// variable => we write same value and same variable and we can write reassing the value 
+// let => we dont write same variable name 
+// const => wr dont write same variable  name we can dont reassing the value 
+
+let x = 4 , y = 19;
+console.log(x + y)
+
+let v ;
+v = 20;
+console.log(v)
+
+let t ;
+console.log(t)
