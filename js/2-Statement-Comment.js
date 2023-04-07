1465cc0a1d7f900c6eb67ee0a31fda2fd54f04d9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-08 02:03:22 +0530

    Statement-and-Comment

---
+++ js/2-Statement-Comment.js
@@ -0,0 +1,12 @@
+ var a;
+ a=3;
+ alert(a);
+ //Statement => we use samicolumn ; and the line that is called statement
+
+ //comment => we comment single line then use double slice//
+//  var a ;
+
+/* we comment multiple line  code  we use  slice start and 
+ star slice */ /* */
+ /*a = 4;
+ alert(a)*/
