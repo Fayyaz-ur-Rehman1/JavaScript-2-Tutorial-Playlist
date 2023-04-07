ad84ba6f860a4a77963f32188345960690c90a79
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-08 04:04:21 +0530

    Operators

---
+++ js/4-Operators.js
@@ -0,0 +1,48 @@
+// Assigment Opreaters
+let x = 20;
+y=30;
+x = y
+console.log(x);
+
+let u = 2;
+u = u * 3;
+// same as
+u *= 3;
+console.log(u);
+
+let i = 4;
+i += 2; // -=  , =% , =* , =/
+console.log(i);
+
+let o =  3;
+p = o ** 2;
+console.log(p);
+
+let q =  3;
+z = q ** 3;
+console.log(z);
+
+let a = 2;
+let b = "2";
+let d = 2
+console.log(a + b);
+console.log(a + " "+b )
+console.log(a + b + d)
+console.log(a + b - d)
+
+let c = "my name is =>";
+let j = "Fayyazurrehman";
+console.log(c + " " + j)
+
+let l = "3";
+// l = l + 1 // 31
+// l++; // 4v 
+console.log(l)
+
+let m = 3;
+let k = m + 3/3;
+console.log(k)
+
+let w = 3;
+let f = (w + 3)/3;
+console.log(f)
