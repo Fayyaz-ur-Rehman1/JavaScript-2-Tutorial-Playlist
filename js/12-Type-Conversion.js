0d220ab802e4cd51f05d159e441a10954b490deb
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 03:13:35 +0530

    Type-Conversion

---
+++ js/12-Type-Conversion.js
@@ -0,0 +1,81 @@
+// Auto metic Conversion
+
+let num = 14 + null;
+console.log(num);
+console.log(typeof num);
+
+let str = 14 + "null";
+console.log(str);
+console.log(typeof str);
+
+let num1 = 14 * "null";
+console.log(num1);
+console.log(num1);
+
+let num2 = true - "23";
+console.log(num2);
+console.log(typeof num2);
+
+let str1 = "14" + 23;
+console.log(str1);
+console.log(typeof str1);
+
+let num3 = "yes" * "no";
+console.log(num3);
+console.log(typeof num3);
+
+// Type of Conversion
+// 1. String
+// 2. Number
+// 3. Boolean
+
+// String
+
+let num4 = 23;
+console.log(str3);
+console.log(typeof str3);
+var str3 = String(num4)
+console.log(typeof str3)
+
+let boolean = true;
+console.log(boolean);
+console.log(typeof boolean);
+var str4 = String(boolean);
+console.log(typeof str4);
+
+// Number
+
+let str2 = "8374";
+console.log(str2);
+console.log(typeof str2);
+var num5 = Number(str2);
+console.log(num5)
+console.log(typeof num5)
+
+let boolean1 = true;
+console.log(boolean);
+console.log(typeof boolean1);
+var num6 = Number(boolean1);
+console.log(typeof num6)
+
+// Boolean
+
+let num7 = 24;
+console.log(num7);
+console.log(typeof num7);
+var boolean3 = Boolean(num7);
+console.log(boolean3);
+
+let str6 = "true";
+console.log(str6);
+console.log(typeof str6);
+var boolean5 = Boolean(str6)
+console.log(boolean5);
+console.log (typeof boolean5);
+
+let str8 = "";
+console.log(str8);
+console.log(typeof str8);
+var boolean9 = Boolean(str8)
+console.log(boolean9);
+console.log (typeof boolean9);
