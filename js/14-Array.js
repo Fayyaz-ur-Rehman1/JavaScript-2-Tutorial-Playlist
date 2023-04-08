467df98d08c5489bc774a1f1fb4ab817d90e664e
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 04:05:43 +0530

    Array

---
+++ js/14-Array.js
@@ -0,0 +1,22 @@
+// let book1 = "Math";
+// let book2 = "Physics";
+// let book3 = "Bio";
+// let book4= "Computer Science";
+// console.log(book3)
+
+
+let books = ["Math","Physics","Bio","Computer Science"];
+console.log(books[3]); 
+
+let book = new Array("Math","Physics","Bio","Computer Science");
+console.log(book)
+
+let num = [1,2,3,4,5,6,7,16,37,74]
+console.log(num[1])
+
+let num1 = [1,2,3,'Math',7,16,37,74]
+console.log(num1[3])
+
+let booksall = ["Math","Physics","Bio","Computer Science"];
+booksall[2] = "English"
+console.log(booksall[2]); 
