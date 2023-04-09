6ef3244202c3eff4ac37b888b4d6b9619eeedadc
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 14:09:01 +0530

    Immediately-invoked-function

---
+++ js/22-Immediately-invoked-function.js
@@ -0,0 +1,14 @@
+(function (){
+    console.log("hello world !");
+    alert("hello world !");
+})();
+
+let string = "Hello";
+
+(function (){
+    let string = "Hello";
+
+    console.log(string);
+    alert(string)
+})();
+
