3a7a6f066fbbc06e43594f0feee86c9b6b55efae
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 14:09:42 +0530

    Object-in-JavaScript

---
+++ js/23-Object-in-JavaScript.js
@@ -0,0 +1,7 @@
+// What is Object 
+// Object is a any this like a man is object car is object and many itms are
+// object but property is like men hed and height and wait etc;
+// teacher is a object then student is a property and value is a subject
+// in JavaScript lauange any method we can use that is called object 
+// that is call object;
+// And you seen Next File and you underStand object in deatail
