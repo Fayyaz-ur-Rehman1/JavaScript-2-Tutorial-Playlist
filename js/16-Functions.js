f91cae538fa5b08b86de412e9490c3c8286abd1a
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 10:39:37 +0530

    Function

---
+++ js/16-Functions.js
@@ -0,0 +1,31 @@
+// for (i = 2; i <= 20; i = i + 2) {
+//     console.log(i);
+// };
+
+// for (i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} = ${2 * i}`);
+// };
+
+function multiple() {
+
+    for (i = 1; i <= 10; i++) {
+        console.log(`2 x ${i} = ${2 * i}`);
+    };
+  
+}
+
+multiple()
+console.log("Hello");
+multiple()
+console.log("my Table");
+multiple()
+
+
+function name(){
+    console.log('Fayyazurrehman,Danishchouhan,Uzairkhilji');
+}
+name()
+name()
+name()
+name()
+name()
