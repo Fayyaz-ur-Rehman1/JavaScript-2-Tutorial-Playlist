184ff42f97d57bbe3e84c5b62912a00f43f1774b
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 14:07:59 +0530

    Anonymous-Function

---
+++ js/21-Anonymous-Function.js
@@ -0,0 +1,19 @@
+// Anonymous Function
+
+// function Expression 
+
+let show = function (){
+    console.log("my name is fayyaz urrrehman");
+};
+show();
+
+// anonymous function
+
+(function (){
+    console.log("my name is fayyazurrehman");
+})
+()
+
+setTimeout((function (){
+    console.log("my name is fayyazurrehman");
+}),4000)
