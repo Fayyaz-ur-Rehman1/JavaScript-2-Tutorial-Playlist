7fffd0fbf386c19e3f3aa5e3ecc090d2540608e5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-09 10:40:17 +0530

    Parameter-Argument

---
+++ js/17-Parameter-and-Argument.js
@@ -0,0 +1,13 @@
+// function Table(num) {
+//     for (i = 1; i <= 10; i++) {
+//         console.log(`${num} x ${i} = ${num * i}`)
+//     }
+// }
+// Table(3);
+// Table(5);
+// Table(4);
+
+function name1(a , b , c){
+    console.log(a + ' ' + b + ' ' + c);
+}
+name1("Fayyazurrehman","DanishChouhan","UzairKhilji");
