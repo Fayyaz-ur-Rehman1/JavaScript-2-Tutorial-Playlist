84a2ef7da6b0ce2368b7ae782800f3e40b03c44c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-10 07:18:45 +0530

    new-Keywords

---
+++ js/30-new-keyword.js
@@ -0,0 +1,11 @@
+// let person = {
+//     fname : "fayyaz",
+//     lname : "khilji",
+//     age : 17
+// }
+// console.log(person.fname);
+
+let person = new Object();
+person.name = "fayyaz"
+person.age = 14;
+console.log(person.age)
