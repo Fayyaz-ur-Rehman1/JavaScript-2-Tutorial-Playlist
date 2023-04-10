4207c915344978090fee0b5fa7284062ad46caac
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-10 07:17:39 +0530

    Random-Number

---
+++ js/28-Random-Number.js
@@ -0,0 +1,18 @@
+let h = Math.random();
+console.log(h);
+
+let i = Math.random() * 10 + 1;
+console.log(i);
+
+let j = Math.random() * 10 + 1;
+console.log(j);
+
+let k = Math.trunc(Math.random() * 101 +1);
+console.log(k);
+
+
+let l = Math.floor(Math.random() * 101 +1);
+console.log(l);
+
+let m = Math.floor(Math.random() * (30 - 20)) + 20;
+console.log(m);
