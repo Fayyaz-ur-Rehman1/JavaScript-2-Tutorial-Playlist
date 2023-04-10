d3da29223c0aa85d8c111f7881385ef7d619e7dc
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-10 07:17:09 +0530

    Math-Object

---
+++ js/27-Math-Object.js
@@ -0,0 +1,23 @@
+let a = Math.PI ;
+console.log(a);
+
+let b = Math.E ;
+console.log(b);
+
+let c = Math.ceil(15.76);
+console.log(c);
+
+let d = Math.floor(15.34) ;
+console.log(d);
+
+let e = Math.round(23.6);
+console.log(e);
+
+let f = Math.min(23 ,43, 12, 6 );
+console.log(f);
+
+let g = Math.max(12,4,12,43,5,2,4,757,5,7,757);
+console.log(g);
+
+let h = Math.trunc(84.34);
+console.log(h)
