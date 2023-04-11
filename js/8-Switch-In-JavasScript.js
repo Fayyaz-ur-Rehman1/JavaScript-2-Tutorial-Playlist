6c5d8399df6c9c31a9e0f131ff317dcc881017ff
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-11 08:59:37 +0530

    Switch-In JavaScript

--- js/8-Switch-In-JavasScript.js
+++ js/8-Switch-In-JavasScript.js
@@ -2,23 +2,23 @@
 // if input = 0 , "n" , "no" otuput = End...
 
 let input;
-input = "n";
+input = 1;
 
-// if (input === 1) {
-//     document.write("continue...")
-// } else if (input === "y") {
-//     document.write("continue...")
-// } else if (input === "yes") {
-//     document.write("continue...")
-// } else if (input === 0) {
-//     document.write("End...")
-// } else if (input === "n") {
-//     document.write("End...")
-// } else if (input === "no") {
-//     document.write("End...")
-// } else {
-//     document.write("worng value")
-// }
+if (input === 1) {
+    document.write("continue...");
+} else if (input === "y") {
+    document.write("continue...");
+} else if (input === "yes") {
+    document.write("continue...");
+} else if (input === 0) {
+    document.write("End...");
+} else if (input === "n") {
+    document.write("End...");
+} else if (input === "no") {
+    document.write("End...");
+} else {
+    document.write("worng value");
+}
 
 
 if (input === 1 || input === "y" || input === "yes") {
@@ -32,48 +32,48 @@ if (input === 1 || input === "y" || input === "yes") {
 
 // switch case compersion ===
 
-// switch (input) {
-//     case 1:
-//         document.write("continue...");
-//         break;
+switch (input) {
+    case 1:
+        document.write("continue...");
+        break;
 
-//     case "y":
-//         document.write("continue...");
-//         break;
+    case "y":
+        document.write("continue...");
+        break;
 
-//     case "yes":
-//         document.write("continue...");
-//         break;
+    case "yes":
+        document.write("continue...");
+        break;
 
-//     case 0:
-//         document.write("End...");
-//         break;
+    case 0:
+        document.write("End...");
+        break;
 
-//     case "n":
-//         document.write("End...");
-//         break;
+    case "n":
+        document.write("End...");
+        break;
 
-//   case "no":
-//     document.write("End...")
-//     break;
+  case "no":
+    document.write("End...")
+    break;
 
-//     default:
-//         document.write("worng value");
-// }
+    default:
+        document.write("worng value");
+}
 
-// switch (input) {
-//     case 1:
-//     case "y":
-//     case "yes":
-//         document.write("continue...");
-//         break;
+switch (input) {
+    case 1:
+    case "y":
+    case "yes":
+        document.write("continue...");
+        break;
 
-//     case 0:
-//     case "n":
-//     case "no":
-//         document.write("End...");
-//         break;
+    case 0:
+    case "n":
+    case "no":
+        document.write("End...");
+        break;
 
-//     default:
-//         document.write("worng value");
-// }
+    default:
+        document.write("worng value");
+}
