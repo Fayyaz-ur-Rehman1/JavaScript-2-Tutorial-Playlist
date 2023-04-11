ed3e1e0262a2a395bcb91f5946b1c7b76936a89f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-11 09:02:36 +0530

    Hoisting

---
+++ js/35-Hoisting.js
@@ -0,0 +1,45 @@
+// "use strict"
+
+fn()
+function fn(){
+    console.log("my name is fayyaz")
+}
+fn()
+
+
+var x;
+x = 7;
+console.log(x);
+
+
+
+a = 4;
+console.log(a);
+var a;
+
+
+var b;
+console.log(b);
+b = 4;
+
+
+var x;
+x = 42
+console.log(x);
+
+// hoisting => kya krta h ke javascript me ka default behevior kya h ke 
+// sare ke sare decleraition ko top me move krdeta h current scope ke hisab 
+// se current scope ka matlab hova ke current file me decleration h matlab ki 
+// global variable h tu vo global scope me rakhega agr ap kisi function ke ander
+// define kya h tu function ke ander se uper leke jayaiga 
+
+
+
+let y ;
+y=4;
+console.log(y);
+
+
+// var t;
+t = 98;
+console.log(t)
