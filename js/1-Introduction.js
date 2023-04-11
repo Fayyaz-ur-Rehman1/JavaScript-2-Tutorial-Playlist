52812242f55ac1ba6b9c784f7c2581c1d8425c25
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-11 08:55:23 +0530

    Introduction

--- js/1-Introduction.js
+++ js/1-Introduction.js
@@ -1,7 +1,8 @@
-alert("my name is fayyazurrehman")
+// alert("my name is fayyazurrehman")
   
   // document.getElementById("h").innerHTML = "JavaScript-Tutorial-||";
    document.write("fayyazurrehman")
-   console.log("hello world")
+   console.log("hello world") //=> console is use you and me check the arrow and 
+  // warning you see output and and write code in console
    console.log(5+6);
    alert(5+5)
