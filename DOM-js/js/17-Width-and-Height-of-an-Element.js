5e707ec758e075069ecf2b78aedf23a274db4ff6
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-14 09:26:45 +0530

    DOM-WORK

---
+++ DOM-js/js/17-Width-and-Height-of-an-Element.js
@@ -0,0 +1,14 @@
+ let a = document.getElementById("box2");
+ let b = a.offsetWidth;
+ let c = a.offsetHeight;
+
+ console.log(b);
+ console.log(c);
+
+
+let d = document.getElementById("box2");
+let e = d.clientHeight;
+let f = d.clientWidth;
+
+console.log(e);
+console.log(f); 
