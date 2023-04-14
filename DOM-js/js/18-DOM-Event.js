43692a73cc3e7ff8e9bbb9d20e4573819c02453f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-14 09:27:23 +0530

    DOM-WORK

---
+++ DOM-js/js/18-DOM-Event.js
@@ -0,0 +1,31 @@
+function fn() {
+    alert("Button Was onclick");
+}
+
+
+function fn1() {
+    alert("Button was click");
+}
+let listner = document.getElementById("listner");
+listner.addEventListener('click', fn1)
+
+
+let listner2 = document.getElementById("listner2");
+listner2.addEventListener('click', function () {
+    alert("This is Anonymous fuction with eventlistener")
+})
+
+
+function fn2() {
+    console.log("Button was click");
+}
+let multiplelistner = document.getElementById("multiple");
+multiplelistner.addEventListener('click', fn2);
+
+multiplelistner.addEventListener("mouseover", function (){
+    console.log("Button was MouseOver")
+});
+
+multiplelistner.addEventListener("mouseout", function (){
+    console.log("Button was MouseOut")
+});
