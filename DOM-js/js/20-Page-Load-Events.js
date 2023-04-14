1f28b78348cd1e9570165e451940d9a4ba5d7d10
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-14 09:28:46 +0530

    Page-Load-Evenet

---
+++ DOM-js/js/20-Page-Load-Events.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded",function(){
+    console.log("DOM tree Create")
+});
+
+window.addEventListener("load" , function(){
+    console.log("Fully loaded");
+});
+
+let img1 = document.getElementById("img1");
+img1.addEventListener("load", function(){
+    console.log("Fully loaded");
+});
