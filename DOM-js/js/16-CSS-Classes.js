8ca1d3a660fddc776e3a5c82f52a5f34be6c6442
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-14 09:25:49 +0530

    CSS-Classes

--- DOM-js/js/16-CSS-Classes.js
+++ DOM-js/js/16-CSS-Classes.js
@@ -1,31 +1,39 @@
 // let p = document.getElementById("box");
 // console.log(p.className);
-// p.className += "dim";
+// p.className += " dim";
 
-let q = document.getElementById("box");
-console.log(q.classList);
+// let q = document.getElementById("box");
+// console.log(q.classList);
 
 
-let q1 = document.getElementById("box");
-let p1 =  q1.classList;
+// let q1 = document.getElementById("box");
+// let p1 =  q1.classList;
 
-for(let x = 0 ; x < p1.length ; x++){
-    console.log(p1[x]);
-}
+// for(let x = 0 ; x < p1.length ; x++){
+//     console.log(p1[x]);
+// }
 
-// let r = document.getElementById("box");
-// r.classList.add("dim")
+// let r2 = document.getElementById("box");
+// r2.classList.add("background")
 
 // let r1 = document.getElementById("box");
-// r1.classList.remove("dim")
+// r1.classList.remove("background")
 
-// let r = document.getElementById("box");
-// r.classList.replace("color" , "dim")
+// let r3 = document.getElementById("box");
+// r3.classList.replace("background" , "Hello")
+ 
+// let r4 = document.getElementById("box");
+// let  l = r4.classList.contains("color");
+// console.log(l)
 
-let r = document.getElementById("box");
-let  l = r.classList.contains("color");
-console.log(l)
+// let r5 = document.getElementById("box");
+// let  l1 = r5.classList.contains("dimaaa");
+// console.log(l1)
 
-let r1 = document.getElementById("box");
-let  l1 = r1.classList.contains("dim");
-console.log(l1)
+
+// let r6 = document.getElementById("box");
+// r6.classList.toggle("dim")
+
+
+let r7 = document.getElementById("box");
+r7.classList.toggle("dim")
