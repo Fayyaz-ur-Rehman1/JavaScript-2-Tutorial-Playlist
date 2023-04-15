45d8f18ba715c1676ac1eaf32b0b8faa9a7a4632
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-16 02:23:40 +0530

    BOM-WORK

---
+++ BOM-js/js/5-Navigator-Object.js
@@ -0,0 +1,9 @@
+// Navigator
+
+console.log(navigator.appName);
+console.log(navigator.appCodeName);
+console.log(navigator.appVersion);
+console.log(navigator.platform);
+console.log(navigator.javaEnabled());
+console.log(navigator.cookieEnabled);
+console.log(navigator.userAgent);
