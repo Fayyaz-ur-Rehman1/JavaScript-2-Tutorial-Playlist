8d475358a8feda7809ab2bf994be018bbeb2650f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-16 02:22:16 +0530

    DOM-WORK

---
+++ BOM-js/js/4-Location-Object.js
@@ -0,0 +1,28 @@
+// console.log(location.href);
+// console.log(location.pathname);
+// console.log(location.protocol);
+
+
+// ridirect
+
+function  fn(){
+    window.location = "https://www.google.com";
+};
+
+
+function  fn1(){
+    location.href = "https://www.google.com";
+};
+
+
+function  fn2(){
+    location.assign("https://www.google.com");
+};
+
+function fn3(){
+    location.replace("https://www.google.com");
+};
+
+function fn4(){
+    location.reload();
+};
