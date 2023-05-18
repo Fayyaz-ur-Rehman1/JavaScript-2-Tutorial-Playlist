ba39864a4770ee08cc90f50e948880d331833f2b
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-18 09:50:45 +0530

    Dom-Practice

---
+++ parctice.js
@@ -0,0 +1,8 @@
+//    id Select 
+
+let elm = document.getElementById("first");
+console.log(elm);
+console.log(elm.innerHTML);
+
+let elm1 = document.getElementById("first");
+elm1.innerHTML = "<h1>FayyazSahab</h1>";
