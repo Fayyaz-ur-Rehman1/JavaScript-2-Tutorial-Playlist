68f4dce0170267de7a1e01c6bbabbbfc25e2003f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-18 10:32:32 +0530

    Dom practice

--- parctice.js
+++ parctice.js
@@ -1,4 +1,5 @@
-//    id Select 
+//    id Select
+
 
 let elm = document.getElementById("first");
 console.log(elm);
@@ -8,8 +9,7 @@ let elm1 = document.getElementById("first");
 elm1.innerHTML = "<h1>FayyazSahab</h1>";
 
 
-
-    //   Select an Element by "Class Name"
+//   Select an Element by "Class Name"
 
 
 let cl = document.getElementsByClassName("cl");
@@ -22,4 +22,20 @@ let mulcl = document.getElementsByClassName("mul cl");
 
 for (i = 0; i < mulcl.length; i++) {
     mulcl[i].innerHTML = "<h1>Ayyaz-Sahab</h1>"
+}
+
+
+// Select an elemrnt by "Tag Name"
+
+
+let tag = document.getElementsByTagName("p");
+for (let i = 0; i < tag.length; i++) {
+    tag[i].innerHTML = "Hello Guys I am Fayyazurrehman";
+}
+
+let div1 = document.getElementById("div1");
+let tagchose = div1.getElementsByTagName("h2");
+
+for (let i = 0; i < tagchose.length; i++) {
+    tagchose[i].innerHTML = "my name is FayyazUrRehman";
 }
