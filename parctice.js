b19ef9946ad2b1348b08d2abd44c4683f6e717f9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-18 10:12:07 +0530

    Dom practice

--- parctice.js
+++ parctice.js
@@ -6,3 +6,20 @@ console.log(elm.innerHTML);
 
 let elm1 = document.getElementById("first");
 elm1.innerHTML = "<h1>FayyazSahab</h1>";
+
+
+
+    //   Select an Element by "Class Name"
+
+
+let cl = document.getElementsByClassName("cl");
+
+for (i = 0; i < cl.length; i++) {
+    cl[i].innerHTML = "<p>Danish Chouhan </p>"
+}
+
+let mulcl = document.getElementsByClassName("mul cl");
+
+for (i = 0; i < mulcl.length; i++) {
+    mulcl[i].innerHTML = "<h1>Ayyaz-Sahab</h1>"
+}
