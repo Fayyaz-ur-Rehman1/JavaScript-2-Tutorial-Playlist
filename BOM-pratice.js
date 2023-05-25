2586febe38576bd79d2b880213446ad75b7e47a2
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-25 11:38:56 +0530

    BOM-Practice-Work

--- BOM-pratice.js
+++ BOM-pratice.js
@@ -1,12 +1,12 @@
-// BOM
+// // BOM
 
-// window Object
+// // window Object
 
 var a = "fayyazurrehman";
 console.log(window.a);
 
 
-function fn(){
+function fn() {
     console.log("i am fayyazurrehman");
 }
 window.fn();
@@ -21,7 +21,7 @@ console.log(window.innerWidth);
 console.log(window.outerHeight);
 console.log(window.outerWidth);
 
-// window
+// // window
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -42,3 +42,60 @@ btn3.addEventListener("click", function () {
     win.close();
 })
 
+
+// Time out and Time Interval
+
+let set = setTimeout(mytimefn, 2000);
+
+function mytimefn() {
+    console.log("Time is Over");
+    alert("Time is Over")
+}
+
+clearTimeout(set)
+
+
+
+let Interval = setInterval(myfn, 2000);
+
+function myfn() {
+    console.log("Time is Start");
+}
+
+let btn4 = document.getElementById("btn4");
+btn4.addEventListener('click', function () {
+    clearInterval(Interval);
+})
+
+
+
+// Location Object
+
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.protocol);
+
+function fun() {
+    window.location = "https://www.google.com"
+    location.href = "https://www.google.com"
+    location.assign("https://www.google.com")
+    location.replace("https://www.google.com")
+    location.reload();
+}
+
+
+// Navigetor
+
+console.log(navigator.appVersion);
+console.log(navigator.appCodeName);
+console.log(navigator.cookieEnabled);
+console.log(navigator.javaEnabled());
+console.log(navigator.userAgent);
+console.log(navigator.platform);
+
+
+// screenn h w
+
+console.log(screen.colorDepth);
+console.log(screen.width);
+console.log(screen.height);
